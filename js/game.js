4276bdb054bb39f9d7202f2e64a3c802fd824e69
wesperjac <jackvanboom@cox.net>
2012-03-13 22:58:11 +0000

    'changed sprite to 40px and added 40px sprite'

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ window.onload = function() {
 	Crafty.canvas();
 	
 	//turn the sprite map into usable components
-	Crafty.sprite(16, "img/sprite.png", {
+	Crafty.sprite(40, "img/sprite40.png", {
         
 		grass1: [0,0],
 		grass2: [1,0],
@@ -13,7 +13,7 @@ window.onload = function() {
 		flower: [0,1],
 		bush1: [0,2],
 		bush2: [1,2],
-		player: [0,3]
+		player: [0,4]
 	});
     Crafty.c  ("Unit", {
         _xPos : 0,
